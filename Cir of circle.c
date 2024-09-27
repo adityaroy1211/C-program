3721b76f8ff8c6ff01f7fc2179fa5cd864c20ab0
adityaroy1211 <royreigns1211@gmail.com>
2024-09-27 16:03:05 +0530

    Add files via upload

---
+++ Cir of circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float r,c;
+	const float pie=3.14;
+	printf("enter the radius of the circle",r);
+	scanf("%f",&r);
+	c=2*pie*r;
+	printf("circumference of the circle is equal to: %f \n",c);
+}
